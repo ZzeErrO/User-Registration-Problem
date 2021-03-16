825c90bf457ae939416cfcd87695ec31d394b749
prashikjaware <prashikjaware123@gmail.com>
2021-03-16 19:27:13 +0530

    [Prashik]Added: Using Try Catch Block to Handle Exceptions in REGEX

--- User_Registration_Problem/UserRegistration/Program.cs
+++ User_Registration_Problem/UserRegistration/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace UserRegistration
 {
@@ -10,29 +11,98 @@ namespace UserRegistration
         public static string email;
         public static string mobileNumber;
         public static string password;
+
+        private static List<string> list1 = new List<string>()
+        {
+            "abc@yahoo.com",
+            "abc-100@yahoo.com",
+            "abc.100@yahoo.com",
+            "abc111@abc.com",
+            "abc-100@abc.net",
+            "abc.100@abc.com.au",
+            "abc@1.com",
+            "abc@gmail.com.com",
+            "abc+100@gmail.com"
+        };
+
         public static bool UserName(string userName) 
         {
-            return Regex.IsMatch(userName, @"^[A-Z]{1}[a-z0-9A-Z]{2,}$");
+            try
+            {
+                if (Regex.IsMatch(userName, @"^[A-Z]{1}[a-z0-9A-Z]{2,}$"))
+                    return true;
+                else
+                    throw new UserRegistrationException(UserRegistrationException.ExceptionTypes.INVALID_FIRST_NAME, "Wrong First Name Entered");
+            }
+            catch (UserRegistrationException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
         }
 
         public static bool LastName(string lastName)
         {
-            return Regex.IsMatch(lastName, @"^[A-Z]{1}[a-z0-9A-Z]{2,}$");
+            try 
+            {
+                if (Regex.IsMatch(lastName, @"^[A-Z]{1}[a-z0-9A-Z]{2,}$"))
+                    return true;
+                else
+                    throw new UserRegistrationException(UserRegistrationException.ExceptionTypes.INVALID_LAST_NAME, "Wrong Last Name Entered");
+            }
+            catch (UserRegistrationException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
         }
 
         public static bool Email(string email)
         {
-            return Regex.IsMatch(email, @"^[A-Z0-9a-z]+([.#_+-][A-Z0-9a-z]+)*[@][A-Z0-9a-z]+([.][A-Za-z]{2,3}){1,2}$");
+            try
+            {
+                if (Regex.IsMatch(email, @"^[A-Z0-9a-z]+([.#_+-][A-Z0-9a-z]+)*[@][A-Z0-9a-z]+([.][A-Za-z]{2,3}){1,2}$"))
+                    return true;
+                else
+                    throw new UserRegistrationException(UserRegistrationException.ExceptionTypes.INVALID_EMAIL, "Wrong Email Entered");
+            }
+            catch (UserRegistrationException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
         }
 
         public static bool MobileNumber(string mobileNumber)
         {
-            return Regex.IsMatch(mobileNumber, @"^[0-9]{2}[ ][0-9]{10}$");
+            try
+            {
+                if (Regex.IsMatch(mobileNumber, @"^[0-9]{2}[ ][0-9]{10}$"))
+                    return true;
+                else
+                    throw new UserRegistrationException(UserRegistrationException.ExceptionTypes.INVALID_PHONE_NUMBER, "Wrong Phone Number Entered");
+            }
+            catch (UserRegistrationException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
         }
-        
+
         public static bool Password(string password)
         {
-            return Regex.IsMatch(password, @"^(?=.*[A-Z])(?=.*[0-9])(?=[^.#+*/$@!%^&_-]*[.#+*/$@!%^&_-][^.#+*/$@!%^&_-]*$)[A-Za-z0-9.#+*/$@!%^&_-]{8,}$");
+            try
+            {
+                if (Regex.IsMatch(password, @"^(?=.*[A-Z])(?=.*[0-9])(?=[^.#+*/$@!%^&_-]*[.#+*/$@!%^&_-][^.#+*/$@!%^&_-]*$)[A-Za-z0-9.#+*/$@!%^&_-]{8,}$"))
+                    return true;
+                else
+                    throw new UserRegistrationException(UserRegistrationException.ExceptionTypes.INVALID_PASSWORD, "Wrong Password Entered");
+            }
+            catch (UserRegistrationException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
         }
 
         static void Main(string[] args)
@@ -48,7 +118,13 @@ namespace UserRegistration
             Console.WriteLine("Enter a Valid Email: ");
             email = Console.ReadLine();
             Console.WriteLine(Email(email) + "\n");
+            Console.WriteLine("Email checking Examples test: \n");
 
+            foreach (var item in list1)
+            {
+                Console.WriteLine(Email(item));
+            }
+   
             Console.WriteLine("Enter a Valid Mobile Number: ");
             mobileNumber = Console.ReadLine();
             Console.WriteLine(MobileNumber(mobileNumber) + "\n");
---
+++ User_Registration_Problem/UserRegistration/UserRegistrationException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    class UserRegistrationException : Exception
+    {
+        public enum ExceptionTypes
+        { 
+            INVALID_FIRST_NAME,
+            INVALID_LAST_NAME,
+            INVALID_EMAIL,
+            INVALID_PHONE_NUMBER,
+            INVALID_PASSWORD
+        }
+        private ExceptionTypes Type;
+
+        public UserRegistrationException(ExceptionTypes type, string message) : base(message)
+        {
+            this.Type = type;
+        }
+    }
+}
